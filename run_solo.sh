5a5ada40e23c6d8b71a8bc5a223536a936f7e378
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2022-06-13 12:11:06 +0100

    update solo

--- run_solo.sh
+++ run_solo.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-ENV=${1:-local}
-BROWSER=${2:-chrome}
-DRIVER=
+#!/bin/bash -e
+DEFAULT_BROWSER=chrome
+BROWSER_TYPE=$1
+ENV=$2
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
+if [ -z "$BROWSER_TYPE" ]; then
+    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+    echo ""
 fi
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER -Daccessibility.test=true "testOnly uk.gov.hmrc.test.ui.cucumber.runner.SoloRunner"
+# Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
+sbt scalafmtCheckAll scalafmtSbtCheck
+
+sbt -Dbrowser="${BROWSER_TYPE:=$DEFAULT_BROWSER}" -Denvironment="${ENV:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.SoloRunner"
